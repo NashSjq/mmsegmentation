44b3c28ee1827459c258440d6a70686dd3f3816a
sjq <17837680831@163.com>
2024-08-20 18:23:20 +0800

    新建自定义数据集文件

---
+++ mmseg/datasets/cityscapes_crack.py
@@ -0,0 +1,23 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from mmseg.registry import DATASETS
+from .basesegdataset import BaseSegDataset
+
+
+@DATASETS.register_module()
+class CityscapesDatasetCrack(BaseSegDataset):
+    """Cityscapes dataset.
+
+    The ``img_suffix`` is fixed to '_leftImg8bit.png' and ``seg_map_suffix`` is
+    fixed to '_gtFine_labelTrainIds.png' for Cityscapes dataset.
+    """
+    METAINFO = dict(
+        classes=('background', 'crack_hor', 'crack_ver', 'crack'),
+        palette=[[128, 64, 128], [244, 35, 232], [70, 70, 70], [102, 102, 156]])
+
+    def __init__(self,
+                 img_suffix='.jpg',
+                 seg_map_suffix='.png',
+                 **kwargs) -> None:
+        super().__init__(
+            img_suffix=img_suffix, seg_map_suffix=seg_map_suffix, **kwargs)
+        
