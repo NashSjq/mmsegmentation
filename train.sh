e14801cf689d1f8600c3506808fff001aaffdf3c
sjq <17837680831@163.com>
2024-08-20 18:20:11 +0800

    训练脚本

---
+++ train.sh
@@ -0,0 +1,4 @@
+python tools/train.py \
+    configs/_myconfig/ocrnet_hr48_4xb2-160k_cityscapes-512x1024.py \
+    --work-dir /home/veily/1sjq/WorkCode/mmsegmentation/out_train/ocrnet_hr48_4xb2-160k_cityscapes-512x1024_roadCrack
+
