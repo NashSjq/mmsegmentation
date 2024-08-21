69c88d6d7fa1019e17dd92b468681050568164c0
sjq <17837680831@163.com>
2024-08-21 18:46:59 +0800

    统一存放运行脚本

---
+++ commands/eval.sh
@@ -0,0 +1,18 @@
+
+# python tools/analysis_tools/analyze_logs.py \
+#     /home/veilytech/1sjq/Train_result/MMSegm/ocrnet_hr48_4xb2-160k_cityscapes-512x1024/20230831_185407/vis_data/20230831_185407.json \
+#     --keys loss \
+#     --legend loss
+
+
+python tools/analysis_tools/analyze_logs.py \
+    /home/veilytech/1sjq/Train_result/MMSegm/ocrnet_hr48_4xb2-160k_cityscapes-512x1024/20230728_095605/vis_data/20230728_095605.json \
+    --keys mIoU mAcc aAcc \
+    --legend mIoU mAcc aAcc
+
+
+# 2 # /home/veilytech/1sjq/Train_result/MMSegm/ocrnet_hr48_4xb2-160k_cityscapes-512x1024/20230726_141802/vis_data/20230726_141802.json \
+# 3 # /home/veilytech/1sjq/Train_result/MMSegm/ocrnet_hr48_4xb2-160k_cityscapes-512x1024/20230802_170931/vis_data/20230802_170931.json
+# 5 # /home/veilytech/1sjq/Train_result/MMSegm/ocrnet_hr48_4xb2-160k_cityscapes-512x1024/20230831_185407/vis_data/20230831_185407.json
+
+
--- commands/train.sh
+++ commands/train.sh
@@ -1,4 +1,3 @@
 python tools/train.py \
     configs/_myconfig/ocrnet_hr48_4xb2-160k_cityscapes-512x1024.py \
     --work-dir /home/veily/1sjq/WorkCode/mmsegmentation/out_train/ocrnet_hr48_4xb2-160k_cityscapes-512x1024_roadCrack
-
